959f0545cc6e860e7c7bf2beeb9cbd63cd0c7085
psin010309-art <psin010309@gmail.com>
2026-08-24 13:32:03 +0900

    Update longest-valid-parentheses.cpp

--- hyeongsin/longest-valid-parentheses.cpp
+++ hyeongsin/longest-valid-parentheses.cpp
@@ -17,13 +17,14 @@ public:
             }
             else
             {
+                //')' 일 경우 top 제거
                 stack.pop();
-                
+                //스택이 비어있다면 새로운 기준점으로 설정.
                  if(stack.empty())
                     {
                         stack.push(ix);
                     }
-                    
+                    //스택이 채워져 있다면 완전한 괄호라고 봐도 되기 때문에 최댓값 구하기.
                     else
                     {
                         answer = max(answer, ix - stack.top());
