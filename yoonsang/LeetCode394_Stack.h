07ef8ac3c160df58c85fb247298dd031079745a8
yoonaward <yoonawardj@gmail.com>
2026-08-25 17:53:03 +0900

    Leetcode 394. Decode String

---
+++ yoonsang/LeetCode394_Stack.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include <string>
+#include <stack>
+#include <cctype>
+
+using namespace std;
+
+/*
+https://leetcode.com/problems/decode-string/description/?envType=problem-list-v2&envId=stack
+LeetCode.394 Decode String
+인코딩된 문자열이 주어졌을 때, 디코딩된 문자열을 반환합니다.
+인코딩 규칙은 다음과 같습니다. k[encoded_string], 여기서 encoded_string대괄호 안의 는 정확히 k번 반복됩니다. 는 k양의 정수임이 보장됩니다.
+입력 문자열은 항상 유효하다고 가정할 수 있습니다. 불필요한 공백이 없고, 대괄호의 형식이 올바르다는 등의 조건을 만족합니다. 
+또한, 원본 데이터에는 숫자가 포함되어 있지 않으며, 숫자는 반복되는 숫자에만 존재한다고 가정할 수 있습니다 . 
+예를 들어, 또는 k와 같은 입력은 없을 것입니다 .3a2[4]
+테스트 케이스는 출력 길이가 절대 초과하지 않도록 생성됩니다 .105
+
+
+예시 1:
+입력: s = "3[a]2[bc]"
+출력: "aaabcbc"
+예시 2:
+
+입력: s = "3[a2[c]]"
+출력: "accaccacc"
+예시 3:
+
+입력: s = "2[abc]3[cd]ef"
+출력: "abcabccdcdcdef"
+
+
+제약 조건:
+
+1 <= s.length <= 30
+s소문자 영문자, 숫자 및 대괄호로 구성됩니다 '[]'.
+s유효한 입력 값임이 보장됩니다 .
+의 모든 정수는 s범위 내에 있습니다 [1, 300].
+
+*/
+
+class Solution
+{
+public:
+    string decodeString(string s)
+    {
+        stack<char>stack;
+        string str = "";
+
+        for (char c : s)
+        {
+            stack.push(c);
+        }
+
+        while (!stack.empty())
+        {
+            if (stack.top() == ']')
+            {
+                stack.pop();
+            }
+
+            else if (isalpha(stack.top()))
+            {
+                str += stack.top();
+
+                stack.pop();
+            }
+
+            else if (stack.top() == '[')
+            {
+                stack.pop();
+
+                if (isdigit(stack.top()))
+                {
+                    // 숫자 만큼 반복
+                    int replay = stack.top() - '0';
+
+                    string previousStr = str;
+
+                    stack.pop();
+
+                    for (int ix = 0; ix < replay - 1; ++ix)
+                    {
+                        str += previousStr;
+                    }
+                }
+            }
+            else
+            {
+                stack.pop();
+            }
+        }
+        reverse(str.begin(), str.end());
+        return str;
+    }
+};
